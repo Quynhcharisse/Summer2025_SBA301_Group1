34d403e337517c24c4d3fa09679162ac60909139
Pham Van Nhu Quynh <vannhuquynhp@gmail.com>
2025-06-16 00:23:37 +0700

    fix: fix error api in ClasseesService and error import in Class

--- pes/client/ui/src/services/SyllabusService.jsx
+++ pes/client/ui/src/services/SyllabusService.jsx
@@ -1,54 +1,54 @@
-import api from '../config/APIConfig.jsx';
+import axiosClient from "../config/APIConfig.jsx";
 
 const SyllabusService = {
   getAll: async () => {
-    const response = await api.get('/syllabus');
+    const response = await axiosClient.get('/syllabus');
     return response.data;
   },
 
   getById: async (id) => {
-    const response = await api.get(`/syllabus/${id}`);
+    const response = await axiosClient.get(`/syllabus/${id}`);
     return response.data;
   },
 
   create: async (data) => {
-    const response = await api.post('/syllabus', data, {
+    const response = await axiosClient.post('/syllabus', data, {
       headers: { 'Content-Type': 'application/json' }
     });
     return response.data;
   },
 
   update: async (id, data) => {
-    const response = await api.put(`/syllabus/${id}`, data, {
+    const response = await axiosClient.put(`/syllabus/${id}`, data, {
       headers: { 'Content-Type': 'application/json' }
     });
     return response.data;
   },
 
   remove: async (id) => {
-    const response = await api.delete(`/syllabus/${id}`);
+    const response = await axiosClient.delete(`/syllabus/${id}`);
     return response.data;
   },
 
   getAllLessons: async () => {
-    const response = await api.get('/lesson');
+    const response = await axiosClient.get('/lesson');
     return response.data;
   },
 
   getLessons: async (syllabusId) => {
-    const response = await api.get(`/syllabus/${syllabusId}/lessons`);
+    const response = await axiosClient.get(`/syllabus/${syllabusId}/lessons`);
     return response.data;
   },
 
   addLesson: async (syllabusId, lessonData) => {
-    const response = await api.post(`/syllabus/${syllabusId}/lessons`, lessonData, {
+    const response = await axiosClient.post(`/syllabus/${syllabusId}/lessons`, lessonData, {
       headers: { 'Content-Type': 'application/json' }
     });
     return response.data;
   },
 
   removeLesson: async (syllabusId, lessonId) => {
-    const response = await api.delete(`/syllabus/${syllabusId}/lessons/${lessonId}`);
+    const response = await axiosClient.delete(`/syllabus/${syllabusId}/lessons/${lessonId}`);
     return response.data;
   }
 };
